bb29ab3b81a45e28394d1d360f90c50cb6023470
Jorge I. Meza <jimezam@gmail.com>
2018-05-17 12:05:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Import the required modules
+////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Database configuration
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Create the Express Application
+////////////////////////////////////////////////////////////////////////
+
+const app = express();
+
+// Configure the server
+////////////////////////////////////////////////////////////////////////
+
+// Parse requests of content-type - "application/x-www-form-urlencoded"
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Parse requests of content-type - "application/json"
+app.use(bodyParser.json())
+
+// Listening server port
+var port = process.env.PORT || 3000;
+
+
+// Define the routes
+////////////////////////////////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+    res.json({
+        "message": "This is a JSON response to a HTTP GET request."
+    });
+});
+
+require('./app/routes/product.routes.js')(app);
+
+// Start the server with selected configuration
+////////////////////////////////////////////////////////////////////////
+
+// Connect to the database
+mongoose.connect(dbConfig.url)
+.then(() => {
+    console.log("Connect to database: success!");    
+}).catch(err => {
+    console.log('Connect to database: failure!');
+    process.exit();
+});
+
+app.listen(port, () => {
+    console.log("Server is listening on port " + port);
+});
